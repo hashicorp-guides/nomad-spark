027f8c660e8bc9bdba589c0506446c504b44f5ea
Andrew Klaas <andrewklaas@Andrews-MacBook-Pro.local>
2017-07-28 17:01:52 -0500

    update

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "nomad" {
   backend = "local"
 
   config {
-    path = "${path.module}/../../nomad/terraform-aws/terraform.tfstate"
+    path = "${path.module}/../nomad/nomad.tfstate"
   }
 }
 
